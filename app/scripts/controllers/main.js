cb35a3ebfbbf601b1e8f4ddb2bdf732eb5d9581e
stalkert <taranenko.yura@gmail.com>
2016-04-01 03:36:33 +0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appTaskBoardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appTaskBoardApp
+ */
+angular.module('appTaskBoardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
